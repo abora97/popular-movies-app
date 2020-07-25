bb0924cf4c018bae4840b29e1b073aa0d46b4250
Abora <https://github.com/abora97>
2020-07-25 04:17:25 +0200

    add model files

---
+++ app/src/main/java/com/abora/popularmoviesapp/model/Movie.java
@@ -0,0 +1,216 @@
+
+
+package com.abora.popularmoviesapp.model;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Movie implements Parcelable {
+
+    @SerializedName("vote_count")
+    @Expose
+    private int voteCount;
+    @SerializedName("id")
+    @Expose
+    private int id;
+    @SerializedName("video")
+    @Expose
+    private boolean video;
+    @SerializedName("vote_average")
+    @Expose
+    private float voteAverage;
+    @SerializedName("title")
+    @Expose
+    private String title;
+    @SerializedName("popularity")
+    @Expose
+    private float popularity;
+    @SerializedName("poster_path")
+    @Expose
+    private String posterPath;
+    @SerializedName("original_language")
+    @Expose
+    private String originalLanguage;
+    @SerializedName("original_title")
+    @Expose
+    private String originalTitle;
+    @SerializedName("genre_ids")
+    @Expose
+    private List<Integer> genreIds = null;
+    @SerializedName("backdrop_path")
+    @Expose
+    private String backdropPath;
+    @SerializedName("adult")
+    @Expose
+    private boolean adult;
+    @SerializedName("overview")
+    @Expose
+    private String overview;
+    @SerializedName("release_date")
+    @Expose
+    private String releaseDate;
+    private boolean favorite;
+
+    public Movie() {
+    }
+
+    public Movie(int voteCount,
+                 int id,
+                 boolean video,
+                 float voteAverage,
+                 String title,
+                 float popularity,
+                 String posterPath,
+                 String originalLanguage,
+                 String originalTitle,
+                 String backdropPath,
+                 boolean adult,
+                 String overview,
+                 String releaseDate,
+                 boolean favorite) {
+        this.voteCount = voteCount;
+        this.id = id;
+        this.video = video;
+        this.voteAverage = voteAverage;
+        this.title = title;
+        this.popularity = popularity;
+        this.posterPath = posterPath;
+        this.originalLanguage = originalLanguage;
+        this.originalTitle = originalTitle;
+        this.backdropPath = backdropPath;
+        this.adult = adult;
+        this.overview = overview;
+        this.releaseDate = releaseDate;
+        this.favorite = favorite;
+    }
+
+    public Movie(boolean favorite) {
+        this.favorite = favorite;
+    }
+
+    public int getVoteCount() {
+        return voteCount;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public boolean hasVideo() {
+        return video;
+    }
+
+    public float getVoteAverage() {
+        return voteAverage;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public float getPopularity() {
+        return popularity;
+    }
+
+    public String getPosterPath() {
+        return posterPath;
+    }
+
+    public String getOriginalLanguage() {
+        return originalLanguage;
+    }
+
+    public String getOriginalTitle() {
+        return originalTitle;
+    }
+
+    public List<Integer> getGenreIds() {
+        return genreIds;
+    }
+
+    public String getBackdropPath() {
+        return backdropPath;
+    }
+
+    public boolean isAdult() {
+        return adult;
+    }
+
+    public String getOverview() {
+        return overview;
+    }
+
+    public String getReleaseDate() {
+        return releaseDate;
+    }
+
+    public boolean isFavorite() {
+        return favorite;
+    }
+
+    public void setFavorite(boolean favorite) {
+        this.favorite = favorite;
+    }
+
+    // Parcelable implementation
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeInt(this.voteCount);
+        dest.writeInt(this.id);
+        dest.writeByte(this.video ? (byte) 1 : (byte) 0);
+        dest.writeFloat(this.voteAverage);
+        dest.writeString(this.title);
+        dest.writeFloat(this.popularity);
+        dest.writeString(this.posterPath);
+        dest.writeString(this.originalLanguage);
+        dest.writeString(this.originalTitle);
+        dest.writeList(this.genreIds);
+        dest.writeString(this.backdropPath);
+        dest.writeByte(this.adult ? (byte) 1 : (byte) 0);
+        dest.writeString(this.overview);
+        dest.writeString(this.releaseDate);
+        dest.writeByte(this.favorite ? (byte) 1 : (byte) 0);
+    }
+
+    protected Movie(Parcel in) {
+        this.voteCount = in.readInt();
+        this.id = in.readInt();
+        this.video = in.readByte() != 0;
+        this.voteAverage = in.readFloat();
+        this.title = in.readString();
+        this.popularity = in.readFloat();
+        this.posterPath = in.readString();
+        this.originalLanguage = in.readString();
+        this.originalTitle = in.readString();
+        this.genreIds = new ArrayList<Integer>();
+        in.readList(this.genreIds, Integer.class.getClassLoader());
+        this.backdropPath = in.readString();
+        this.adult = in.readByte() != 0;
+        this.overview = in.readString();
+        this.releaseDate = in.readString();
+        this.favorite = in.readByte() != 0;
+    }
+
+    public static final Creator<Movie> CREATOR = new Creator<Movie>() {
+        @Override
+        public Movie createFromParcel(Parcel source) {
+            return new Movie(source);
+        }
+
+        @Override
+        public Movie[] newArray(int size) {
+            return new Movie[size];
+        }
+    };
+}
---
+++ app/src/main/java/com/abora/popularmoviesapp/model/MoviesResponse.java
@@ -0,0 +1,40 @@
+
+
+package com.abora.popularmoviesapp.model;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+import java.util.List;
+
+public class MoviesResponse {
+
+    @SerializedName("page")
+    @Expose
+    private int page;
+    @SerializedName("total_results")
+    @Expose
+    private int totalResults;
+    @SerializedName("total_pages")
+    @Expose
+    private int totalPages;
+    @SerializedName("results")
+    @Expose
+    private List<Movie> movies = null;
+
+    public int getPage() {
+        return page;
+    }
+
+    public int getTotalResults() {
+        return totalResults;
+    }
+
+    public int getTotalPages() {
+        return totalPages;
+    }
+
+    public List<Movie> getMovies() {
+        return movies;
+    }
+}
